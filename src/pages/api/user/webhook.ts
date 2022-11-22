2ca2d0e619b1ae0c5a4e3f1a8c675ab3c0fe31e6
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2022-11-22 18:21:08 -0500

    chore: rename api for user webhook, add docs

--- src/pages/api/user/webhook.ts
+++ src/pages/api/user/webhook.ts
@@ -2,11 +2,20 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { Webhook, WebhookUnbrandedRequiredHeaders } from "svix";
 import { ClerkPayload, CreatedUserData, DeletedUserData } from "types/clerk";
 import isClerkEvent from "utils/is-clerk";
-import { createNewUser, deleteUser } from "../../prisma/create-new-user";
+import { createNewUser, deleteUser } from "../../../prisma/create-new-user";
 
 const secret = process.env.clerkWebhookSecret as string;
 
-export default async function handler(
+/**
+ * Middleware handler for incoming webhook requests for user-related events from Clerk.
+ * See more {@link https://clerk.dev/docs/integration/webhooks here}.
+ *
+ * @remarks webhook signature is verified by {@link https://docs.svix.com/receiving/verifying-payloads/how Svix}
+ * @param req Next API request
+ * @param res Next API response
+ *
+ */
+export default async function userWebhookHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -34,7 +43,9 @@ export default async function handler(
     }
 
     return res.status(200).json({
-      message: `successfully processed ${verifiedPayload?.type}`,
+      message: `successfully processed ${
+        verifiedPayload?.type ?? verifiedPayload.event_type
+      }`,
     });
   } catch (error) {
     return res.status(400).json(error);
